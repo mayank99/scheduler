96a80ac5c0fea434123d3aaf01ccdd531c3c3c23
mayank99 <9084735+mayank99@users.noreply.github.com>
2019-02-24 20:31:39 -0600

    Finished a basic scheduler implementation

--- process.c
+++ process.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "lib/integer.h"
+#include "lib/queue.h"
 
 typedef enum { READY, PAUSED, BLOCKED, FINISHED, KILLED } STATE;
 
@@ -20,16 +22,17 @@ typedef struct {
 
 int random_between(int from, int to) { return (rand() % (to - from + 1)) + from; }
 
-process *open_process(int n) {
+process *open_process(int n, char *mode) {
   process *p = malloc(sizeof(process));
   assert(p != 0);
 
   char s[16];
-  sprintf(s, "p%i.txt", n);
-  FILE *fp = fopen(s, "r+");
+  sprintf(s, "processes/p%i.txt", n);
+  FILE *fp = fopen(s, mode);
   // if (!fp) damn("file never opened :/");
   assert(fp != 0);
 
+  p->index = n;
   p->fp = fp;
   p->priority = 0;
   p->position = 0;
@@ -37,26 +40,6 @@ process *open_process(int n) {
   p->time_left = 0;
   p->state = READY;
   return p;
-}
-
-void generate_process(int n) {
-  // open file number n
-  process *p = open_process(n);
-
-  // TODO: metadata for process
-  fprintf(p->fp, "%9i\n", random_between(0, 3));              // priority
-  fprintf(p->fp, "%9i\n", 0);                                 // position
-  fprintf(p->fp, "%9i\n", random_between(10, 200));           // memory
-  fprintf(p->fp, "%9i\n", random_between(400000, 40000000));  // execution time
-  fprintf(p->fp, "%9i\n", 0);                                 // start time
-  fprintf(p->fp, "%9i\n", 0);                                 // end time
-
-  // actual instructions
-  for (int i = 0; i < random_between(1000, 100000); i++) {
-    fprintf(p->fp, "%3i\n", random_between(1, 20));
-  }
-
-  fclose(p->fp);
 }
 
 void close_process(process *p) {
@@ -78,7 +61,7 @@ void close_process(process *p) {
 }
 
 void read_process_metadata(process *p) {
-  int priority, position, time_left;
+  int priority, position, memory, time_left, start_time, end_time;
 
   fseek(p->fp, 0, SEEK_SET);
   fscanf(p->fp, "%i", &priority);
@@ -86,18 +69,52 @@ void read_process_metadata(process *p) {
   fseek(p->fp, 10, SEEK_SET);
   fscanf(p->fp, "%i", &position);
 
+  fseek(p->fp, 20, SEEK_SET);
+  fscanf(p->fp, "%i", &memory);
+
   fseek(p->fp, 30, SEEK_SET);
   fscanf(p->fp, "%i", &time_left);
 
+  fseek(p->fp, 40, SEEK_SET);
+  fscanf(p->fp, "%i", &start_time);
+
+  fseek(p->fp, 50, SEEK_SET);
+  fscanf(p->fp, "%i", &end_time);
+
   p->priority = priority;
   p->position = position;
+  p->memory = memory;
   p->time_left = time_left;
+  p->start_time = start_time;
+  p->end_time = end_time;
+}
+
+void generate_process(int n) {
+  // open file number n
+  process *p = open_process(n, "w+");
+
+  // TODO: metadata for process
+  fprintf(p->fp, "%9i\n", random_between(0, 3));              // priority
+  fprintf(p->fp, "%9i\n", 0);                                 // position
+  fprintf(p->fp, "%9i\n", random_between(10, 200));           // memory
+  fprintf(p->fp, "%9i\n", random_between(400000, 40000000));  // execution time
+  fprintf(p->fp, "%9i\n", 0);                                 // start time
+  fprintf(p->fp, "%9i\n", 0);                                 // end time
+
+  // actual instructions
+  for (int i = 0; i < random_between(1000, 100000); i++) {
+    fprintf(p->fp, "%3i\n", random_between(1, 20));
+  }
+
+  // fclose(p->fp);
+  read_process_metadata(p);
+  close_process(p);
 }
 
 // process p to be ran for quantum time
 int execute_process(process *p, int quantum) {
   // read metadata
-  read_process_metadata(p);
+  // read_process_metadata(p);
 
   int count = 0;  // counting time
   int i = 0;      // numbers read from file (execution time to add)
@@ -110,7 +127,11 @@ int execute_process(process *p, int quantum) {
       break;
     }
 
-    if (rand() % 50 == 0) {  // 2% chance for block
+    count += i;
+    p->position++;
+    p->time_left -= i;
+
+    if (rand() % 100 >= 98) {  // 2% chance for block
       // 90% chance for 4-100 ms, 10% for 100-100000 ms
       int x = (rand() % 9) > 0 ? (rand() % 9) + 2 : (rand() % 316) + 10;
       int delay = x * x * 1000;  // exponential delay in us
@@ -118,10 +139,6 @@ int execute_process(process *p, int quantum) {
       p->state = BLOCKED;
       break;
     }
-
-    count += i;
-    p->position++;
-    p->time_left -= i;
 
     if (p->time_left <= 0) {
       p->state = KILLED;
@@ -139,26 +156,44 @@ int execute_process(process *p, int quantum) {
 }
 
 int main() {
-  // for (int i = 0; i < 1; i++) {
-  //   generate_process(i);
-  // }
+  printf("p_number start_time end_time state\n");
+  printf("-----------------------------------\n");
   srand(time(0));
-  generate_process(0);
 
-  int timer = 0;
+  int timer = 0;  // cpu clock
+  int new_process_at = 0;
+  int process_count;
 
-  for (int i = 0; i < 10000; i++) {
-    process *p = open_process(0);
+  QUEUE *q = newQUEUE();
+  setQUEUEfree(q, freeINTEGER);
+  setQUEUEdisplay(q, displayINTEGER);
+
+  // while (1) {
+  do {
+    // issue new process every 20-10000 ms
+    if (timer == 0 || timer > new_process_at && process_count <= 10) {
+      generate_process(process_count++);
+      enqueue(q, newINTEGER(process_count - 1));
+      // if (process_count == 5) break;
+      new_process_at = timer + random_between(20, 10000);
+    }
+
+    process *p = open_process(getINTEGER(dequeue(q)), "r+");
     read_process_metadata(p);
 
     if (p->state == READY || p->state == PAUSED) {
       p->start_time = timer;
       timer += execute_process(p, 10000);
       p->end_time = timer;
-    } else {
-      break;
     }
 
+    // if (p->state == FINISHED || p->state == KILLED)
+    if (p->state == FINISHED)
+      printf("%7i %10i %8i %6i\n", p->index, p->start_time, p->end_time, p->state);
+
+    if (p->state == BLOCKED && timer >= p->unblock_time) p->state = PAUSED;
+    if (p->state == PAUSED || p->state == BLOCKED) enqueue(q, newINTEGER(p->index));
+
     close_process(p);
-  }
+  } while (sizeQUEUE(q) > 0);
 }
